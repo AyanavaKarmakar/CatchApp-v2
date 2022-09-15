4531ad2dfea3f1461177fe452b8cf874b886a043
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-15 11:41:59 +0530

    feat: setup navigation container

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style='auto' />
-      <Text>Lets build signal!</Text>
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <StatusBar style='auto' />
+        <Text>Lets build signal!</Text>
+      </View>
+    </NavigationContainer>
   )
 }
 
