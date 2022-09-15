cc6bc7d7a0d1bda19fe30eb4b795a31aff9f1c4b
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-15 11:45:12 +0530

    feat: comments

--- App.tsx
+++ App.tsx
@@ -2,6 +2,9 @@ import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 
+/**
+ * Refer = https://reactnavigation.org/docs/getting-started/#wrapping-your-app-in-navigationcontainer
+ */
 export default function App() {
   return (
     <NavigationContainer>
