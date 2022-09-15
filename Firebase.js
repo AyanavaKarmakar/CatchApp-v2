290233afb048d53907c3b195af017c699f055b46
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-15 18:49:30 +0530

    feat: configue firestore

--- Firebase.js
+++ Firebase.js
@@ -1,3 +1,10 @@
+/**
+ * @see https://docs.expo.dev/guides/using-firebase/
+ */
+import * as firebase from 'firebase'
+import 'firebase/firestore'
+import 'firebase/auth'
+
 const firebaseConfig = {
   apiKey: 'AIzaSyCHVyXsveLBH6o1bnJ31RJ28304rAaUT-E',
   authDomain: 'signal-clone-af25b.firebaseapp.com',
@@ -6,3 +13,16 @@ const firebaseConfig = {
   messagingSenderId: '254420441430',
   appId: '1:254420441430:web:89edb275fb7961871a5ef7',
 }
+
+let firebaseApp
+
+if (firebase.apps.length === 0) {
+  firebaseApp = firebase.initializeApp(firebaseConfig)
+} else {
+  firebaseApp = firebase.app()
+}
+
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+
+export { db, auth }
