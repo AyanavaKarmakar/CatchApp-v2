40cc188670f8315968a152a09ca72b4408cc9743
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-15 11:37:00 +0530

    feat: setup firebase config

---
+++ Firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCHVyXsveLBH6o1bnJ31RJ28304rAaUT-E',
+  authDomain: 'signal-clone-af25b.firebaseapp.com',
+  projectId: 'signal-clone-af25b',
+  storageBucket: 'signal-clone-af25b.appspot.com',
+  messagingSenderId: '254420441430',
+  appId: '1:254420441430:web:89edb275fb7961871a5ef7',
+}
