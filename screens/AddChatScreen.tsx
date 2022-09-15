20336290a77d3e6280e4cf654bdbb774394b3599
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-16 01:44:11 +0530

    save point

--- screens/AddChatScreen.tsx
+++ screens/AddChatScreen.tsx
@@ -1,9 +1,54 @@
-import { View, Text } from 'react-native'
+import { useLayoutEffect, useState } from 'react'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { View, StyleSheet } from 'react-native'
+import { NativeRootStackParamList } from '../App'
+import { Button, Icon, Input } from '@rneui/themed'
+import { collection } from '@firebase/firestore'
+/**
+ * @see https://reactnavigation.org/docs/typescript/#type-checking-screens
+ */
+type Props = NativeStackScreenProps<NativeRootStackParamList, 'ADDCHAT'>
 
-export const AddChatScreen = () => {
+export const AddChatScreen = (props: Props) => {
+  const { navigation } = props
+  const [input, setInput] = useState<string>()
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: 'ADD NEW CHAT',
+      headerStyle: { backgroundColor: '#000000' },
+      headerTitleStyle: { color: '#E0FFFF' },
+      headerTitleAlign: 'center',
+    })
+  }, [navigation])
+
+  const handleCreateChat = async () => {
+    await collection('chats')
+      .add({
+        chatName: input,
+      })
+      .then(() => {
+        navigation.goBack()
+      })
+      .catch((error: { code: number; message: string }) => {
+        const errorCode = error.code
+        const errorMessage = error.message
+        alert(`${errorCode} + ${errorMessage}`)
+      })
+  }
+
   return (
-    <View>
-      <Text>Add chat screen</Text>
+    <View style={styles.container}>
+      <Input
+        placeholder='Enter a new chat name'
+        onChangeText={(text) => setInput(text)}
+        leftIcon={<Icon name='wechat' type='antdesign' size={24} color='black' />}
+      />
+      <Button onPress={handleCreateChat} title='Create new chat' />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {},
+})
