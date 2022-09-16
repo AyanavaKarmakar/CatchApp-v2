f280fc8f08d70bec90925da155029f522c94fb24
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-16 23:59:08 +0530

    style: chat screen header

--- screens/ChatScreen.tsx
+++ screens/ChatScreen.tsx
@@ -1,6 +1,28 @@
+import { useLayoutEffect } from 'react'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { View, Text } from 'react-native'
+import { NativeRootStackParamList } from '../App'
 
-export const ChatScreen = () => {
+/**
+ * @see https://reactnavigation.org/docs/typescript/#type-checking-screens
+ */
+type Props = NativeStackScreenProps<NativeRootStackParamList, 'CHAT'>
+
+export const ChatScreen = (props: Props) => {
+  const { navigation } = props
+
+  /**
+   * @see https://reactnavigation.org/docs/navigation-prop#setoptions
+   */
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: 'CHATS',
+      headerStyle: { backgroundColor: '#000000' },
+      headerTitleStyle: { color: '#E0FFFF' },
+      headerTitleAlign: 'center',
+    })
+  }, [navigation])
+
   return (
     <View>
       <Text>Chat Screen</Text>
