23f6663f4b675bc847388181ee23f3c8531fbc0b
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-16 12:03:00 +0530

    feat: get snapshot

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,19 +1,27 @@
-import { useLayoutEffect } from 'react'
+import { useEffect, useLayoutEffect, useState } from 'react'
 import { ImageSourcePropType, SafeAreaView, ScrollView, TouchableOpacity, View } from 'react-native'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { AntDesign, SimpleLineIcons } from '@expo/vector-icons'
 import { NativeRootStackParamList } from '../App'
 import { CustomListItem } from '../components'
 import { Avatar } from '@rneui/themed'
-import { auth } from '../Firebase'
+import { auth, db } from '../Firebase'
+import { doc, DocumentData, onSnapshot } from 'firebase/firestore'
 
 /**
  * @see https://reactnavigation.org/docs/typescript/#type-checking-screens
  */
 type Props = NativeStackScreenProps<NativeRootStackParamList, 'HOME'>
 
+interface Snapshot {
+  id: string
+  data: DocumentData
+}
+
 export const HomeScreen = (props: Props) => {
   const { navigation } = props
+
+  const [chats, setChats] = useState<Snapshot[]>()
 
   const handleAddChat = () => {
     navigation.navigate('ADDCHAT')
@@ -62,6 +70,23 @@ export const HomeScreen = (props: Props) => {
       ),
     })
   }, [navigation])
+
+  /**
+   * @see https://firebase.google.com/docs/firestore/query-data/listen
+   */
+  useEffect(() => {
+    const unsubscribe = onSnapshot(doc(db, 'chats'), (doc) => {
+      setChats([
+        ...(chats || []),
+        {
+          id: doc.id,
+          data: doc.data,
+        },
+      ])
+    })
+
+    return unsubscribe
+  }, [])
 
   return (
     <SafeAreaView>
