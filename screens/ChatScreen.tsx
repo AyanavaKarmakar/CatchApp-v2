391ab835609b840c9fca31038f181edb664b254a
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-17 13:46:11 +0530

    style: personal color scheme

--- screens/ChatScreen.tsx
+++ screens/ChatScreen.tsx
@@ -59,33 +59,35 @@ export const ChatScreen = (props: Props) => {
   }, [navigation])
 
   const sendMessage = async () => {
-    /**
-     * Removes keyboard
-     */
-    Keyboard.dismiss()
+    if (input !== '') {
+      /**
+       * Removes keyboard
+       */
+      Keyboard.dismiss()
 
-    /**
-     * @see https://firebase.google.com/docs/firestore/manage-data/add-data
-     * @see https://stackoverflow.com/a/70551419/18893631
-     */
-    const docRef = doc(db, 'chats', id)
-    const colRef = collection(docRef, 'messages')
-    await addDoc(colRef, {
-      timestamp: serverTimestamp(),
-      message: input,
-      displayName: auth.currentUser?.displayName,
-      email: auth.currentUser?.email,
-      photoURl: auth.currentUser?.photoURL,
-    }).catch((error: { code: number; message: string }) => {
-      const errorCode = error.code
-      const errorMessage = error.message
-      alert(`${errorCode} + ${errorMessage}`)
-    })
+      /**
+       * @see https://firebase.google.com/docs/firestore/manage-data/add-data
+       * @see https://stackoverflow.com/a/70551419/18893631
+       */
+      const docRef = doc(db, 'chats', id)
+      const colRef = collection(docRef, 'messages')
+      await addDoc(colRef, {
+        timestamp: serverTimestamp(),
+        message: input,
+        displayName: auth.currentUser?.displayName,
+        email: auth.currentUser?.email,
+        photoURl: auth.currentUser?.photoURL,
+      }).catch((error: { code: number; message: string }) => {
+        const errorCode = error.code
+        const errorMessage = error.message
+        alert(`${errorCode} + ${errorMessage}`)
+      })
 
-    /**
-     * Resets input field
-     */
-    setInput('')
+      /**
+       * Resets input field
+       */
+      setInput('')
+    }
   }
 
   /**
@@ -151,13 +153,14 @@ export const ChatScreen = (props: Props) => {
             <View style={styles.footer}>
               <TextInput
                 placeholder={`[${chatName}] Start Typing... `}
+                placeholderTextColor='#E0FFFF'
                 onChangeText={(text) => setInput(text)}
                 onSubmitEditing={sendMessage}
                 value={input}
                 style={styles.textInput}
               />
               <TouchableOpacity activeOpacity={0.5} onPress={sendMessage}>
-                <Ionicons name='send' size={24} color='black' />
+                <Ionicons name='send' size={24} color='#E0FFFF' />
               </TouchableOpacity>
             </View>
           </>
@@ -170,7 +173,7 @@ export const ChatScreen = (props: Props) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: '#1F2022',
   },
   viewContainer: {
     flex: 1,
@@ -186,15 +189,15 @@ const styles = StyleSheet.create({
     height: 40,
     width: '90%',
     marginRight: 15,
-    backgroundColor: '#ECECEC',
+    backgroundColor: '#000000',
     padding: 10,
-    color: 'grey',
+    color: '#E0FFFF',
     borderRadius: 30,
     textAlign: 'center',
   },
   receiver: {
     padding: 15,
-    backgroundColor: 'cyan',
+    backgroundColor: '#00f4f4',
     alignSelf: 'flex-end',
     borderRadius: 20,
     marginRight: 15,
@@ -213,7 +216,7 @@ const styles = StyleSheet.create({
   },
   sender: {
     padding: 15,
-    backgroundColor: 'blue',
+    backgroundColor: '#006a6a',
     alignSelf: 'flex-start',
     borderRadius: 20,
     margin: 15,
