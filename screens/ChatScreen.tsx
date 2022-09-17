160a01f39e625232b5f0a7f43eb5168694b262d5
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-17 13:34:54 +0530

    feat: chat setup

--- screens/ChatScreen.tsx
+++ screens/ChatScreen.tsx
@@ -8,6 +8,7 @@ import {
   ScrollView,
   View,
   TextInput,
+  Text,
   TouchableOpacity,
   Keyboard,
   TouchableWithoutFeedback,
@@ -15,20 +16,35 @@ import {
 import { NativeRootStackParamList } from '../App'
 import { StatusBar } from 'expo-status-bar'
 import { Ionicons } from '@expo/vector-icons'
-import { doc, collection, addDoc, serverTimestamp } from 'firebase/firestore'
+import {
+  doc,
+  collection,
+  addDoc,
+  serverTimestamp,
+  query,
+  orderBy,
+  onSnapshot,
+  DocumentData,
+} from 'firebase/firestore'
 import { auth, db } from '../Firebase'
+import { Avatar } from '@rneui/themed'
 
 /**
  * @see https://reactnavigation.org/docs/typescript/#type-checking-screens
  */
 type Props = NativeStackScreenProps<NativeRootStackParamList, 'CHAT'>
 
+interface Messages {
+  id: string
+  data: DocumentData
+}
+
 export const ChatScreen = (props: Props) => {
   const { navigation, route } = props
   const { chatName, id } = route.params
 
   const [input, setInput] = useState<string>()
-  // const [messages, setMessages] = useState([])
+  const [messages, setMessages] = useState<Messages[]>()
 
   /**
    * @see https://reactnavigation.org/docs/navigation-prop#setoptions
@@ -75,23 +91,21 @@ export const ChatScreen = (props: Props) => {
   /**
    * @see https://firebase.google.com/docs/firestore/query-data/order-limit-data
    */
+  useLayoutEffect(() => {
+    const docRef = doc(db, 'chats', id)
+    const colRef = collection(docRef, 'messages')
+    const q = query(colRef, orderBy('timestamp', 'desc'))
+    const unSubscribe = onSnapshot(q, (querySnapshot) => {
+      setMessages(
+        querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        })),
+      )
+    })
 
-  // useLayoutEffect(() => {
-  //   const unSubscribe = collection(db, 'chats')
-  //     .doc(id)
-  //     .collection('messages')
-  //     .orderBy('timestamp', 'desc')
-  //     .onSnapshot((snapshot) => {
-  //       setMessages({
-  //         snapshot.docs.map((doc)=>({
-  //           id: doc.id,
-  //           data: doc.data()
-  //         }))
-  //       })
-  //     })
-
-  //   return unSubscribe
-  // }, [route])
+    return unSubscribe
+  }, [route])
 
   return (
     <SafeAreaView style={styles.container}>
@@ -103,7 +117,37 @@ export const ChatScreen = (props: Props) => {
       >
         <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
           <>
-            <ScrollView>{/* Chat goes here */}</ScrollView>
+            <ScrollView contentContainerStyle={{ paddingTop: 15 }}>
+              {messages?.map((message) =>
+                message.data.email === auth.currentUser?.email ? (
+                  <View key={message.id} style={styles.receiver}>
+                    <Avatar
+                      rounded
+                      containerStyle={styles.receiverAvatar}
+                      size={30}
+                      source={{ uri: message.data.photoURL }}
+                    />
+                    <Text style={styles.receiverText}>{message.data.message}</Text>
+                  </View>
+                ) : (
+                  <View style={styles.sender}>
+                    <Avatar
+                      rounded
+                      containerStyle={styles.senderAvatar}
+                      size={30}
+                      source={{ uri: message.data.photoURL }}
+                    />
+                    <Text style={styles.senderText}>{message.data.message}</Text>
+                    <Text style={styles.senderName}>
+                      {message.data.displayName} |{' '}
+                      {message.data.timestamp
+                        .toDate()
+                        .toLocaleString('en-US', { timeZone: 'Asia/Kolkata' })}
+                    </Text>
+                  </View>
+                ),
+              )}
+            </ScrollView>
             <View style={styles.footer}>
               <TextInput
                 placeholder={`[${chatName}] Start Typing... `}
@@ -147,5 +191,49 @@ const styles = StyleSheet.create({
     color: 'grey',
     borderRadius: 30,
     textAlign: 'center',
+  },
+  receiver: {
+    padding: 15,
+    backgroundColor: 'cyan',
+    alignSelf: 'flex-end',
+    borderRadius: 20,
+    marginRight: 15,
+    marginBottom: 20,
+    maxWidth: '80%',
+    position: 'relative',
+  },
+  receiverText: {
+    color: 'black',
+    fontWeight: '500',
+  },
+  receiverAvatar: {
+    position: 'absolute',
+    bottom: -15,
+    right: -5,
+  },
+  sender: {
+    padding: 15,
+    backgroundColor: 'blue',
+    alignSelf: 'flex-start',
+    borderRadius: 20,
+    margin: 15,
+    maxWidth: '80%',
+    position: 'relative',
+  },
+  senderText: {
+    color: 'white',
+    fontWeight: '500',
+    fontSize: 15,
+    marginBottom: 15,
+  },
+  senderName: {
+    paddingRight: 10,
+    fontSize: 12,
+    color: 'white',
+  },
+  senderAvatar: {
+    position: 'absolute',
+    bottom: -15,
+    left: -5,
   },
 })
