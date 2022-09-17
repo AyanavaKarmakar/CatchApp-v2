b5e298cc467f5d09630a7492896c5abe75e39e9d
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-09-17 14:10:38 +0530

    style: scheme 1

--- components/CustomListItem.tsx
+++ components/CustomListItem.tsx
@@ -1,4 +1,5 @@
 import { ListItem, Avatar } from '@rneui/themed'
+import { StyleSheet } from 'react-native'
 
 interface Props {
   id: string
@@ -13,7 +14,7 @@ export const CustomListItem = (props: Props) => {
     'https://user-images.githubusercontent.com/89210438/190708630-526de943-f158-4f24-809b-279c58ea70fe.png'
 
   return (
-    <ListItem onPress={() => onEnterChat(id, chatName)} key={id} bottomDivider>
+    <ListItem containerStyle={styles.container} onPress={() => onEnterChat(id, chatName)} key={id}>
       <Avatar
         rounded
         source={{
@@ -21,11 +22,27 @@ export const CustomListItem = (props: Props) => {
         }}
       />
       <ListItem.Content>
-        <ListItem.Title style={{ fontWeight: '800' }}>{chatName}</ListItem.Title>
-        <ListItem.Subtitle numberOfLines={1} ellipsizeMode='tail'>
+        <ListItem.Title style={styles.title}>{chatName}</ListItem.Title>
+        <ListItem.Subtitle style={styles.subtitle} numberOfLines={1} ellipsizeMode='tail'>
           This is the chat for admins
         </ListItem.Subtitle>
       </ListItem.Content>
     </ListItem>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#0F0014',
+    marginTop: 5,
+    marginBottom: 5,
+    marginLeft: 5,
+    marginRight: 5,
+    padding: 5,
+    borderRadius: 10,
+    borderColor: '#E0FFFF',
+    borderWidth: 1,
+  },
+  title: { fontWeight: '800', color: '#E0FFFF' },
+  subtitle: { color: 'white' },
+})
